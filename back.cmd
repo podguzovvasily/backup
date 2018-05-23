1d619fde93fc27e7612e7c20cf1969b6634131c4
podguzovvasily <podguzovvasily@gmail.com>
2018-05-23 09:22:26 +0300

    Update back.cmd

--- back.cmd
+++ back.cmd
@@ -1,4 +1,3 @@
-robocopy h:\000 D:\ /e /copyall /zb /XD "System Volume Information" 
-/mt:16 /DCOPY:T /r:10 /W:5 /V /TS /FP /ETA /TEE /LOG:D:\H.txt
+robocopy h:\000 D:\ /e /copyall /zb /XD "System Volume Information" /mt:16 /DCOPY:T /r:10 /W:5 /V /TS /FP /ETA /TEE /LOG:D:\H.txt
 
-robocopy c:\SSD C:\temp /MIR 
+robocopy \\source_IP\source_folder E:\Backups\Destination_folder /MIR /copyall /zb /XD "System Volume Information" /mt:32 /DCOPY:T /r:5 /W:5 /V /TS /FP /ETA /TEE /LOG:E:\Backups\backup.log
